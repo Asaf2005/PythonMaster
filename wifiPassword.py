6eca15a8d57c321cf8b129252c81deb24930135b
AsafSivan1 <asafisivan@gmail.com>
2021-04-13 16:52:16 +0300

    Update wifiPassword.py

--- wifiPassword.py
+++ wifiPassword.py
@@ -1,11 +1,15 @@
-import subprocess, smtplib, re
+import subprocess,, re
 
-command1 = "netsh wlan show profile"
-networks = subprocess.check_output(command1,shell=True)
-network_list = re.findall('(?:profile\s*:\s)(.*)',networks)
+def main():
+	command1 = "netsh wlan show profile"
+	networks = subprocess.check_output(command1,shell=True)
+	network_list = re.findall('(?:profile\s*:\s)(.*)',networks)
 
-output = ""
-for network in network_list:
-	comamnd2 =  "netsh wlan show profile " +network + " key=clear"
-	one_network_result = subprocess.check_output(command2, shell=True)
-	output += one_network_result
+	output = ""
+	for network in network_list:
+		comamnd2 =  "netsh wlan show profile " +network + " key=clear"
+		one_network_result = subprocess.check_output(command2, shell=True)
+		output += one_network_result
+	return output
+if __name__ == "__main__":
+	print(main())
